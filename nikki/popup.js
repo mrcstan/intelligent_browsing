c18f4afe45e3155aa75d6735002fa815da6fa9b7
Xue Ying Lin <nikkiylin@gmail.com>
2021-10-26 18:04:40 -0700

    Disable search button when no text

--- nikki/popup.js
+++ nikki/popup.js
@@ -1,1 +1,11 @@
 console.log('popup!')
+
+window.onload = () => {
+  document.querySelector('#SearchBox').addEventListener('keyup', (event) => {
+    const button = document.querySelector('#SearchButton')
+    console.log('Value is ' + event.target.value)
+    button.disabled = event.target.value.length === 0
+
+    console.log('Text changed!')
+  })
+}
